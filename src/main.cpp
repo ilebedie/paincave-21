b17a9ee58cbe3505f2881c54aa6d368ecc0e162b
Ivan Lebediev <ivan.lebediev@fiverr.com>
2021-12-27 22:20:36 +0200

    how its cpp17

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 using namespace std;
+
+template<typename... Args>
+bool all(Args... args) { return (... && args); }
+ 
+
 int main(){
-    cout<<"Here\n";
+    bool b = all(true, true, true, false);
+    cout<<"Here\n is b: "<<b;
 }
