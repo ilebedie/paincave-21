7e869912a5c0280b2e297671368d5f5c49451c9c
Ivan Lebediev <ivan.lebediev@fiverr.com>
2022-01-02 22:13:00 +0200

    first rendering

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,37 @@ void testCpp17() {
     cout<<"Here\n is b: "<<b;
 }
 
+// to renderer
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// to renderer
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
+// most of this goes to renderer
     glfwInit();
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-
-    GLFWwindow* window = glfwCreateWindow(800, 800, "paincave-21", nullptr, nullptr);
-    if (window == NULL) {
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "paincave-21", nullptr, nullptr);
+    if (window == nullptr) {
         cout<<"failed to create a window\n";
         glfwTerminate();
         return -1;
@@ -31,13 +52,20 @@ int main() {
 
     gladLoadGL();
 
-    glViewport(0,0,800,800);
-
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // swap
+        glfwSwapBuffers(window);
+
+        // poll
         glfwPollEvents();
     }
 
