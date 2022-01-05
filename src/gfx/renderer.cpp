917a030b34e4e7db10b755565cb8a75e20d98263
Ivan Lebediev <ivan.lebediev@fiverr.com>
2022-01-05 23:36:20 +0200

    fix bug with file read - should learn to do it properly

--- src/gfx/renderer.cpp
+++ src/gfx/renderer.cpp
@@ -7,10 +7,12 @@ std::string read_shader_file(const std::string file_path)
 {
     const auto fsize = std::filesystem::file_size(file_path);
     std::ifstream in(file_path, std::ios::binary);
-	std::string contents('0',fsize);
-	in.read(&contents[0], contents.size());
-	in.close();
-	return contents;
+    // TODO: take a look at this abomination later
+    std::string contents;
+    contents.resize(fsize);
+    in.read(&contents[0], contents.size());
+    in.close();
+    return contents;
 }
 
 void load_shaders()
@@ -19,6 +21,9 @@ void load_shaders()
     auto vs_code = read_shader_file(vs_path);
     std::cout<<"vs code: "<<vs_code<<std::endl;
     // const auto fs_path = "res/shaders/default.fs";
+    const auto fs_path = "res/shaders/default.vs";
+    auto fs_code = read_shader_file(fs_path);
+    std::cout<<"fs code: "<<fs_code<<std::endl;
 }
 
 Renderer::Renderer()
